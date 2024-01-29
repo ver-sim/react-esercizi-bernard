d7f6bcb40303672eef566bea3f9a4e55de5d6d8b
ver-sim <verdianasimonetti@gmail.com>
2024-01-29 10:09:39 +0100

    traccia n. 3

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from "react-router-dom"
 import { Card } from "./Card"
 import { Navigation } from "./Navigation"
+import { Login } from "./Login"
 
 const App = () => {
 
@@ -8,12 +9,13 @@ const App = () => {
   return (
     < >
       <Link to='/'>Traccia n. 1</Link>
-      <Link to='/second'>Traccia n. 2</Link>
+      <Link to='/navigation'>Traccia n. 2</Link>
+      <Link to='/login'>Traccia n. 3</Link>
       
       <Routes>
         <Route path="/" element={<Card />}/>
-        <Route path="/second" element={<Navigation />}/>
-
+        <Route path="/navigation" element={<Navigation />}/>
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Link to='/'>Back</Link>
 
